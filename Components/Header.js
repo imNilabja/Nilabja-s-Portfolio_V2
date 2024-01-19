7454d86cd17c82c151d8774e2e6f5483f9d257b4
Nilabja <goswamineel23@gmail.com>
2024-01-19 16:04:39 +0530

    .//

--- Components/Header.js
+++ Components/Header.js
@@ -6,12 +6,12 @@
             <>
                 <div className='flex w-screen h-14 relative'>
 
-                    <p className='w-[10rem] ml-10 mt-4 font-semibold text-red-500 italic font-serif'>Nilabja's Portfolio</p>
+                    <p className='w-[10rem] ml-10 mt-4 font-semibold text-white italic font-serif'>Nilabja's Portfolio</p>
 
                     <div className=' italic font-serif flex justify-between w-[20rem] left-[40rem] relative text-white text-m mt-4'>
                             
                         
-                    <Link href='app/page.js' className=''>Home</Link>
+                    {/* <Link href='app/page.js' className=''>Home</Link> */}
                         <Link href='/Projects' className=''>Projects</Link>
                         <Link href='/Skill' className=''>Skills</Link>
                         <Link href='/About' className=''>About</Link>
