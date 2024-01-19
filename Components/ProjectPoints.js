d5d29517017084cd579025f7afeffe83af1f2b60
Nilabja <goswamineel23@gmail.com>
2024-01-19 20:38:46 +0530

    made project box a little responsive

--- Components/ProjectPoints.js
+++ Components/ProjectPoints.js
@@ -6,13 +6,13 @@ const ProjectPoints = (props) => {
     console.log(download)
     return (
         <>
-            <div className='bg-red-600 w-[950px] min-h-[100px] relative left-[160px] top-[100px] rounded-[25px] p-4 flex-col justify-center'>
-                <p className='bg-orange-600 text-white font-semibold text-2xl w-[900px] min-h-[50px] py-3 px-4 rounded-[20px] left-[9px] relative'>
+            <div className='bg-red-600 max-w-[950px] min-h-[100px] relative left-[160px] top-[100px] rounded-[25px] p-4 flex-col justify-center'>
+                <p className='bg-orange-600 text-white font-semibold text-2xl max-w-[900px] min-h-[50px] py-3 px-4 rounded-[20px] mr-4 left-[9px] relative'>
                     {Topic_name}
                 </p>
                 <br></br>
 
-                <div className='bg-orange-600 min-h text-white font-semibold text-xl w-[900px] min-h-[60px] py-3 px-4 rounded-[20px] left-[9px] relative'>
+                <div className='bg-orange-600 min-h text-white font-semibold text-xl maix-w-[900px] min-h-[60px] py-3 px-4 mr-4 rounded-[20px] left-[9px] relative'>
                     <span className='text-black'>
                         Description:
                     </span>
--- app/Projects/page.js
+++ app/Projects/page.js
@@ -7,17 +7,17 @@ const page = () => {
   return (
     <>
 
-      <div className='bg-gray-800 relative min-h-screen min-w-screen flex flex-col pb-[400px] pr-1 justify-center' >
+      <div className='bg-gray-800 relative min-h-screen min-w-screen flex flex-col pb-[40px] pr-1 justify-center' >
 
-        <div className='relative top-[-500px]'></div>
+        
         <Header />
 
         <p className='text-white font-semibold font-mono w-14 text-4xl left-[540px] top-[50px] relative'>PROJECTS</p>
         {/* <ProjectPoints Topic_name="" desc=" " /> */}
 
 
-       
-        <ProjectPoints Topic_name="IOT BASED FRAMEWORK DESIGN FOR AUTOMATED HUMAN EMOTION RECOGNITION." desc=" This project proposes a novel approach to identifying mental health issues in real-time through the use of physical changes such as sweat capacitance and heart rate. The project leverages cost-efficient and simple-to-arrange sensors, which can be integrated with smartphones and smartwatches to send alerts to medical professionals and families. The proposed model is based on the Internet of Medical Things (IoMT) and machine learning, which not only recognizes emotions but also predicts long-term depression risk through maintained databases. The integration of these technologies could lead to earlier and more accurate diagnosis, as well as better management of mental health conditions. Overall, this project has the potential to improve the lives of individuals with mental health issues and their families, and contribute to the growing field of digital health." download="https://drive.google.com/file/d/1TE00HpQJjqp0lCa0iCFVnuvph4n1KTdm/view?usp=sharing"/>
+
+        <ProjectPoints Topic_name="IOT BASED FRAMEWORK DESIGN FOR AUTOMATED HUMAN EMOTION RECOGNITION." desc=" This project proposes a novel approach to identifying mental health issues in real-time through the use of physical changes such as sweat capacitance and heart rate. The project leverages cost-efficient and simple-to-arrange sensors, which can be integrated with smartphones and smartwatches to send alerts to medical professionals and families. The proposed model is based on the Internet of Medical Things (IoMT) and machine learning, which not only recognizes emotions but also predicts long-term depression risk through maintained databases. The integration of these technologies could lead to earlier and more accurate diagnosis, as well as better management of mental health conditions. Overall, this project has the potential to improve the lives of individuals with mental health issues and their families, and contribute to the growing field of digital health." download="https://drive.google.com/file/d/1TE00HpQJjqp0lCa0iCFVnuvph4n1KTdm/view?usp=sharing" />
         <br></br>
 
         <ProjectPoints Topic_name="AI ENABLED FINTECH B2B INVOICE MANAGEMENT APPLICATION" desc="AI Enabled Fintech B2B Invoice Management Application helps companies automate tasks related to invoice processing. B2B means business to business transaction. In this ideal world, the buyer business should payback within the stipulated time which is the Payment Term. However the buyer business hardly or infrequently pays within their established time frame . This is where the Account receivables Department comes into picture. This Application consists of Machine Learning Model to predict the payment date of an invoice when it gets created in the system, A Receivables Dashboard to visualize data in the form of grids,ADD,EDIT,DELETE,ADVANCE SEARCH functionalities.It has easy navigation implemented through pagination. The tools used are :
