05a504c90cfce779e8143f2f108d5a2001006bc7
Nilabja <goswamineel23@gmail.com>
2024-01-19 15:33:17 +0530

    /////

--- next.config.js
+++ next.config.js
@@ -2,3 +2,8 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+// next.config.js
+const withCSS = require('@zeit/next-css');
+module.exports = withCSS({
+  /* Your other configurations go here */
+});
