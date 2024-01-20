2a3d882e600b5444e2a18181415ec3cfc87809f7
Nilabja <goswamineel23@gmail.com>
2024-01-20 15:45:21 +0530

    responsive 2

--- Components/Header.js
+++ Components/Header.js
@@ -6,15 +6,18 @@ const Header = () => {
         <>
             <div className='flex min-w-[100px] h-14 relative'>
 
-                <p className='w-[200px] ml-10 mt-4 font-semibold text-white italic font-serif'>Nilabja's Portfolio</p>
+                <p className='w-[200px] ml-10 mt-5 font-semibold text-white italic font-serif'>Nilabja's Portfolio</p>
 
                 <div className=' italic font-serif flex justify-evenly w-[450px] h-[60px] ml-auto relative text-white text-m mt-2 py-3'>
 
 
-                    <Link href='/Projects' className='my-2'>Project</Link>
-                    <Link href='/Skill' className='my-2'>Skills</Link>
-                    <Link href='/About' className='my-2'>About</Link>
-                    <Link href='/Contact' className='my-2'>Contact</Link>
+                    <Link href='/Projects' className='my-1'>Project</Link>
+                    <p className='mt-1'>|</p>
+                    <Link href='/Skill' className='my-1'>Skills</Link>
+                    <p className='mt-1'>|</p>
+                    <Link href='/About' className='my-1'>About</Link>
+                    <p className='mt-1'>|</p>
+                    <Link href='/Contact' className='my-1'>Contact</Link>
 
                 </div>
 
