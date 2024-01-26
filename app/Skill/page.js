6595548f91b341cf1201eb4a31c25da2c3028c34
Nilabja <goswamineel23@gmail.com>
2024-01-26 21:42:30 +0530

    bug check 2

--- app/Skill/page.js
+++ app/Skill/page.js
@@ -15,8 +15,9 @@ const page = () => {
         <div className='flex-row justify-center sm:flex-row min-h-[50px] min-w-[100px] max-w-screen relative ml-4 mr-4 mt-10 p-[20px] mb-10'>
           {/* skill add template-><SkillCircle img="/Java-Logo.png" h="1000" w="1000" mt="8" desc=""/> */}
           
+
+          <SkillCircle img="/Java-Logo.png" h="1000" w="1000" mt="[32px]" desc="Experienced in Java, I possess proficiency in Collection Frameworks and Data Structures & Algorithms (DSA), specializing in stack implementations. With a solid foundation, I've demonstrated adeptness in creating efficient and optimized solutions, contributing valuable expertise to Java-based projects. " />
           
-          <SkillCircle img="/Java-Logo.png" h="1000" w="1000" mt="8" desc="Experienced in Java, I possess proficiency in Collection Frameworks and Data Structures & Algorithms (DSA), specializing in stack implementations. With a solid foundation, I've demonstrated adeptness in creating efficient and optimized solutions, contributing valuable expertise to Java-based projects. "/>
           <SkillCircle img="/cpp.png" h="500" w="500" mt="[-10px]" desc="Experienced in C++ with a strong foundation in Standard Template Library (STL) and Data Structures and Algorithms (DSA) expertise, particularly in array manipulation. Proficient in utilizing STL containers and algorithms to efficiently solve complex problems and implement robust solutions. Adept at optimizing array-based operations for enhanced performance in diverse programming scenarios."/>
 
           
