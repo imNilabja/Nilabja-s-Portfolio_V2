4ee0632c817d0755525afac353e02c78015fdc81
Nilabja <goswamineel23@gmail.com>
2024-01-26 22:34:58 +0530

    final adjust

--- Components/SkillCircle.js
+++ Components/SkillCircle.js
@@ -11,14 +11,14 @@ const SkillCircle = (props) => {
       <div className='flex justify-center flex-col sm:flex-row mr-auto ml-auto'>
 
 
-        <div className='bg-orange-400 m-7 h-[220px] w-[220px] justify-center relative rounded-[200px] overflow-hidden mr-10'>
-          <div className='min-h-[20px] min-w-[100px] '>
-            <Image src={img} width={w} height={h} className={`relative top-[${mt}px]`}></Image>
+        <div className=' m-7 h-[220px] w-[220px] relative justify-center rounded-[200px] top-[-20px] mr-10'>
+          <div className='min-h-[60px] min-w-[150px] relative top-10'>
+            <Image src={img} width={w} height={h} className={`relative top-[${mt}]`}></Image>
           </div>
 
         </div>
 
-        <div className='m-7 min-h-[180px] min-w-[200px] max-w-[450px] relative'>
+        <div className='m-7 min-h-[180px] min-w-[200px] max-w-[450px] relative top-[20px]'>
           <div className='m-auto min-h-[20px] min-w-[100px] p-3 text-white font-semibold'>
             {desc}
           </div >
