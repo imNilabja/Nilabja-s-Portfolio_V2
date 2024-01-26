70a8a9d6edf119f8e743e1c2f9cf9bf790ba9fae
Nilabja <goswamineel23@gmail.com>
2024-01-26 20:37:30 +0530

    minor change

--- Components/SkillCircle.js
+++ Components/SkillCircle.js
@@ -8,14 +8,14 @@ const SkillCircle = () => {
       <div className='flex justify-center flex-col sm:flex-row m-10'>
         
 
-      <div className='bg-orange-400 m-auto min-h-[200px] min-w-[200px] relative rounded-[200px] overflow-hidden mr-10'>
+      <div className='bg-orange-400 m-7 min-h-[200px] min-w-[200px] relative rounded-[200px] overflow-hidden mr-10'>
         <div className='m-auto min-h-[20px] min-w-[100px] p-3 '>
                 
         </div>
         
          </div>
 
-         <div className='bg-blue-400 m-auto min-h-[180px] min-w-[200px] mt-4 relative'>
+         <div className='bg-blue-400 m-7 min-h-[180px] min-w-[200px] relative'>
         <div className='m-auto min-h-[20px] min-w-[100px] p-3 '>
                 
         </div>
