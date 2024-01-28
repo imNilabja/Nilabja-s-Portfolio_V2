09581a2568dd8dddce6477265034a5d3cf37975a
Nilabja <goswamineel23@gmail.com>
2024-01-28 15:40:24 +0530

    home page

---
+++ Components/main_pageContent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const main_pageContent = () => {
+  return (
+    <>
+      <div className='w-[600px] mr-auto h-[400px] ml-auto text-white font-semibold text-4xl relative top-40 justify-centers p-2 mt-5 italic font-serif'>Hi,<br></br> I am Nilabja Nayan<br></br> Goswami</div> 
+    </>
+  )
+}
+
+export default main_pageContent
--- app/page.js
+++ app/page.js
@@ -2,14 +2,17 @@
 import React from 'react'
 import Header from '@/Components/Header'
 import Footer from '@/Components/footer'
+import Main from '@/Components/main_pageContent'
 function page() {
 
   return (
     <>
 
-      <div className='bg-gray-800 w-screen h-screen absolute'>
+      <div className='bg-gray-800 relative min-h-screen min-w-screen flex flex-col pr-1 overflow-hidden'>
 
         <Header />
+
+       <Main/>
         <Footer/>
       </div>
 
