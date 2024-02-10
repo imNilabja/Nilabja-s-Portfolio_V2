02272f972bc43ffc982331e8cc20f8ba7edf5623
Nilabja <goswamineel23@gmail.com>
2024-02-11 02:07:04 +0530

    adjustments

--- Components/main_pageContent.js
+++ Components/main_pageContent.js
@@ -9,11 +9,15 @@ const main_pageContent = () => {
         <Image src="/star.png" width={2000} height={2000} className='absolute top-[40px] left-[-250px] w-full opacity-50 rounded-xl' /> */}
 
 
-        <div className=' text-white font-semibold text-4xl relative  top-40 p-2 mt-5 italic font-serif z-50'>Hi, I am Nilabja
+        <div className=' text-teal-300 font-semibold text-4xl relative ml-auto mr-auto top-40 p-2 mt-5 italic font-serif z-50'>Hi, I am Nilabja
           <br></br>
           <div className='text-xl'>A Software Engineer</div>
         </div>
-        <Image src="/coding.png" width={500} height={500} className='absolute top-[-10px] mt-auto w-full opacity-15 rounded-xl' />
+        <Image src="/coding.png" width={500} height={500} className='absolute top-[-10px] mt-auto w-full opacity-20 rounded-xl' />
+        <div className='w-52 h-52 relative left-[550px] top-[250px]'>
+          <Image src="/tag.png" width={50} height={50} className='absolute top-[-10px] mt-auto w-full opacity-20 rounded-xl' />
+
+        </div>
       </div>
     </>
   )
--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ function page() {
         <Header />
 
        <Main/>
-        <Footer/>
+  
       </div>
 
     </>
