b795174ac76d3fadc0bc3eb48044a161218235e5
Nilabja <goswamineel23@gmail.com>
2024-02-17 00:19:40 +0530

    added social media links

--- app/Contact/page.js
+++ app/Contact/page.js
@@ -3,6 +3,7 @@
 import React from 'react'
 import Header from '@/Components/Header'
 import Footer from '@/Components/footer'
+import Image from 'next/image';
 const page = () => {
   return (
     <>
@@ -17,7 +18,7 @@ const page = () => {
 
 
         <form className='flex-col dire justify-evenly relative max-w-[550px] min-h-[100px] ml-auto mr-auto top-28'>
-          <div className='flex justify-center ml-auto mr-auto '>
+          <div className='flex justify-center ml-auto mr-auto'>
             <input type='text' placeholder='ENTER NAME' className='italic text-sm text-white thin min-w-[300px] max-w-[500px] bg-gray-800 border-2 border-yellow-500 rounded-[20px] p-2 ml-auto mr-auto mt-5 mb-5'></input>
           </div>
           <div className='flex justify-center ml-auto mr-auto '>
@@ -31,7 +32,44 @@ const page = () => {
           </div>
 
 
+
         </form>
+
+        <div className=' text-white font-semibold ml-auto mr-auto text-2xl min-w-[20px] max-w-[900px] min-h-[50px] py-3 px-4 rounded-[20px] relative top-[300px] mb-12 justify-evenly'>
+          {/* // */}
+
+          <a href='https://www.linkedin.com/in/nilabjagoswami23/' target='_blank'>
+            <button className=' max-h-[50px] m-3'>
+              <div className='w-1 h-1'>
+                <Image src="/linkedin.png" width={20} height={20} className='absolute mt-auto opacity-100 ' />
+              </div>
+            
+            </button>
+          </a>
+
+          {/* // */}
+
+          <a href='https://www.instagram.com/nilabja23/' target='_blank'>
+            <button className='max-h-[50px] m-10'>
+              <div className='w-1 h-1'>
+                <Image src="/insta.png" width={20} height={20} className='absolute mt-auto opacity-100 ' />
+              </div>
+             
+            </button>
+          </a>
+          {/* // */}
+           
+          <a href='https://github.com/imNilabja' target='_blank'>
+            <button className='max-h-[50px] m-3'>
+              <div className='w-1 h-1'>
+                <Image src="/github.png" width={20} height={20} className='absolute mt-auto opacity-100 rounded-[30px]' />
+              </div>
+             
+            </button>
+          </a>
+
+
+        </div>
 
         <Footer />
       </div>
